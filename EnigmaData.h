6e02fcfd6f892f2ed09f802f04ae8638fb0bda52
Kainomad <kainomad2@gmail.com>
2019-03-09 20:56:12 +0100

    Création d'EnigmaData.h

---
+++ EnigmaData.h
@@ -0,0 +1,20 @@
+#ifndef EnigmaData_h
+#define EnigmaData_h
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+enum class ID_RT {I, II, III, IV, V};
+// RT for Rotor
+
+enum class ID_RF{UKW_A, UKW_B, UKW_C};
+// RF for Reflector
+
+//string RotorWirings[] = {"EKMFLGDQVZNTOWYHXUSPAIBRCJ", "AJDKSIRUXBLHWTMCQGZNPYFVOE", "BDFHJLCPRTXVZNYEIWGAKMUSQO", "ESOVPZJAYQUIRHXLNFTGKDCMWB", "VZBRGITYUPSDNHLXAWMJQOFECK"};
+//string ReflectorWirings[] = {"EJMZALYXVBWFCRQUONTSPIKHGD", "YRUHQSLDPXNGOKMIEBFZCWVJAT", "FVPJIAOYEDRZXWGCTKUQSBNMHL"};
+
+
+
+#endif /* EnigmaData_h */
